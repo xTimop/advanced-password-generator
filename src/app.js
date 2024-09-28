7954174f1e1c171b189f1eceaa0a89ce56cede41
Timop <timopchik@gmail.com>
2024-09-28 11:26:37 +0300

    Make real-time password generation instead of button

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 function hashPassword(event){
-    event.preventDefault()
-
     let password = document.getElementById('password').value;
+    if(password == '') {
+        document.getElementById('result').style.display = 'none';
+        return;
+    };
     let service = document.getElementById('service').value.toLowerCase();
+    if(service == '') {
+        document.getElementById('result').style.display = 'none';
+        return;
+    };
     let info = document.getElementById('info').value.toLowerCase();
 
     res = b85encode(fromHexString(sha3_256(concat3(
